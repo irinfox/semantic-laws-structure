13f4b774026d964956a71c7d6130efcd97cef86a
krylova93@gmail.com <irinfox@users.noreply.github.com>
2014-06-09 22:54:08 +0000

    latest versions of files

--- np.cxx
+++ np.cxx
@@ -8,6 +8,7 @@ ComplexOr -> SimConjAnd Or;
 Conj -> 'и' | 'или' | 'либо' | ComplexOr;
 PRO -> Word<gram="SPRO", kwtype=~[bad_np_start]>; // | Word<gram="APRO">;
 
+//простые именные группы
 NPinBr -> LBracket EnumNP RBracket;
 NP -> Noun<gram="~PR"> | PRO ;
 NP -> NP<rt> Noun<gram=~"nom">+;
@@ -24,19 +25,21 @@ AinBr -> LBracket Adj<rt> RBracket;
 ANP -> (Adv+) Adj<gnc-agr[1]>+ (AinBr) NP<gnc-agr[1], rt>;
 ANP -> (Adv+) Adj<gnc-agr[1]> (AinBr<gnc-agr[1]>) Conj ANP<gnc-agr[1], rt>;
 ANP -> (Adv+) Adj<gnc-agr[1]> ',' ANP<gnc-agr[1], rt>;
-//ANP -> (Adv+) Adj<gnc-agr[1]> 'до' ANP<gnc-agr[1], rt>;
 NP -> Word<gram="NUM"> 'до' NP;
 
+//предложные группы
 PP -> Prep NP<rt>;
 PP -> Prep ReferInt;
 PP -> ReferInt1;
 PPS -> PP+;
 
+//именные и предложные группы с перечислениями
 EnumNP -> NP | PPS;
 EnumNP -> EnumNP<c-agr[1]> Comma EnumNP<c-agr[1]>;
 EnumNP -> EnumNP<c-agr[1]> Conj EnumNP<c-agr[1]>;
 EnumNP -> Prep Adj<gnc-agr[1]> NP Noun<gnc-agr[1]>;
 
+//причастный оборот
 Neg -> 'не';
 Partcp -> (Adv) (Neg) Word<gram="partcp",rt>; 
 Partcp -> Partcp<gnc-agr[1]> Conj Partcp<gnc-agr[1]>;
@@ -47,6 +50,7 @@ PartcpPhrase -> PartcpPhrase Conj PartcpPhrase;
 PartcpPhrase -> Partcp PPS; 
 PartcpPhraseC -> Comma PartcpPhrase (SubStop);
 
+//именные группы, включающие причастные обороты и определительные придаточные
 NP -> NP<c-agr[1]> Conj NP<c-agr[1]>;
 BigNP -> NP;
 BigNP -> PartcpPhrase<gnc-agr[1]> NP<gnc-agr[1]>;
@@ -58,6 +62,7 @@ BigNP -> NP SubordinateC;
 BigNP -> BigNP Conj NP;
 BigNP -> BigNP Comma 'а' 'также' NP;
 
+//определительные придаточные (с "который")
 ComplVerb -> Word<gram="V">+;
 ComplVerb -> ComplVerb Conj ComplVerb;
 ComplVAdv -> (Adv) ComplVerb;
@@ -70,6 +75,7 @@ Subordinate -> Which ComplVAdv EnumNP;
 Subordinate -> (PP) Which NP ComplVAdv (NP);
 SubordinateC -> Comma Subordinate SubStop<cut>;
 
+//другие придаточные
 When -> 'когда';
 SubWhen -> When EnumNP ComplVerb NP;
 SubWhenC -> Comma When EnumNP SubStop;
